9a823e2f5579219d68e843e4b227fb749584f3b9
Robert <rdtheriault@yahoo.com>
2017-09-08 08:22:55 -0700

    Create proxy.js

---
+++ proxy.js
@@ -0,0 +1,16 @@
+////Configure var url or grab it from another location/input
+var url = website.com/something.json
+
+////Change https or http
+getName = "https%3A//"+url;
+//getName = "https%3A//"+url;
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function()  {
+  if (this.readyState == 4 && this.status == 200) {
+    extObj =  JSON.parse(this.responseText);
+    document.getElementById("named").innerHTML = extObj.name;
+  }
+};
+xmlhttp.open("GET", "proxy.php?url="+getName, true);
+xmlhttp.send();
