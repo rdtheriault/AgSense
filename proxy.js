ed4d961fa0604c19b2ea9d1c3b74291283c36ecc
Robert <rdtheriault@yahoo.com>
2017-09-08 08:25:40 -0700

    Update proxy.js
    added where to put code instruction

--- proxy.js
+++ proxy.js
@@ -9,6 +9,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function()  {
   if (this.readyState == 4 && this.status == 200) {
     extObj =  JSON.parse(this.responseText);
+    ////put your json processing code here or call a function
     document.getElementById("named").innerHTML = extObj.name;
   }
 };
